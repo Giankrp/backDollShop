fdec6bcf43dd8c6c913476f78d133f450bf83d99
Giankrp <gianruiz2003@gmail.com>
2023-09-15 17:27:52 -0500

    add cors

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const morgan = require("morgan");
 const { server } = require("./routes/index.js");
 const { sq, testDbConnection } = require("./db/conexion.db.js");
 const Dolls = require("./db/dolls.db.js");
+const cors = require("cors");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const main = express();
+
+main.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: "GET, POST, OPTIONS, PUT, DELETE",
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
+  }),
+);
 
 main.use(morgan("dev"));
 
