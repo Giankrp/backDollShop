3c6272b33d784669e7a197b2a3fce838b1c2c1a2
Giankrp <gianruiz2003@gmail.com>
2023-09-22 19:20:22 -0500

    connection db update

--- db/conexion.db.js
+++ db/conexion.db.js
@@ -5,13 +5,9 @@ dotenv.config();
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-const sequelize = new Sequelize({
-  dialect: "postgres", // Cambia a tu dialecto de base de datos (por ejemplo, mysql, sqlite)
-  host: DB_HOST,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-});
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+);
 
 const testDbConnection = async () => {
   try {
